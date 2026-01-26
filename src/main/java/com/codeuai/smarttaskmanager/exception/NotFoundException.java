563a7eba67f309b60f2a616bc3f4bde9f95d2623
natan machado <natan.machado2014@gmail.com>
2026-01-25 20:28:27 -0500

    Add customized exceptions

---
+++ src/main/java/com/codeuai/smarttaskmanager/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.codeuai.smarttaskmanager.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
